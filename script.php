18e62873efea378e1f8f58183e93c582dbb2b591
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-04-12 16:22:10 +0200

    add comment

--- script.php
+++ script.php
@@ -1,19 +1,22 @@
 <?php 
 
 $phrase = $_POST["phrase"];
-$badWord = $_POST["badWord"];
+$badWord = $_POST["badWord"]; //recuperiamo con la super globale i risultati dei due input
 
+$phraseLength = strlen($phrase); // strlen come .length ci permette di sapere il n. dei caratteri
+
+echo "<p>Frase: $phrase</p>";
+echo "<p>Lunghezza della Frase: $phraseLength caratteri</p>";  //con echo buttiamo in template i risulatti della frase
+
+$badWordLength = strlen($badWord); 
+
+$censorSymbol = str_repeat('*', $badWordLength +1); // con repeat moltiplichiamo l'input * per un numero che è la lunghezza in caratteri aggiunto +1 tanto per divertirmi con la concatenazione
+
+$censored = str_ireplace($badWord, $censorSymbol, $badWord); // con str_ireplace sostituiamo alla prima variabile la seconda nella posizione occupata dalla variabile in cui la vogliamo
+
+$censoredLength = strlen($censored);
+
+echo "<p>Parolaccia: $censored</p>";
+echo "<p>Lunghezza della prolaccia: $censoredLength caratteri</p>";
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
-</body>
-</html>
